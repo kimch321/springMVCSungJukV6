6924a06a4bd7d20c98174adb45c076a9b29d055b
KIM CHUNG IL <cnddlf12@naver.com>
2023-04-25 15:48:27 +0900

    스프링MVC 프로젝트 - 성적 조회2

---
+++ src/main/java/kimch321/spring4/sungjukv6/controller/SungJukController.java
@@ -0,0 +1,30 @@
+package kimch321.spring4.sungjukv6.controller;
+
+import kimch321.spring4.sungjukv6.service.SungJukV6Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SungJukController {
+
+    private SungJukV6Service sjsrv;
+
+    @Autowired
+    public SungJukController(SungJukV6Service sjsrv) {
+        this.sjsrv = sjsrv;
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        System.out.println(sjsrv.readSungJuk());
+
+        // sungjuklist.jsp에 성적조회결과 데이터를 sjs라는 이름으로 넘김
+        model.addAttribute("sjs", sjsrv.readSungJuk());
+        return "sungjuklist";
+    }
+
+}
---
+++ src/main/java/kimch321/spring4/sungjukv6/dao/SungJukV6DAO.java
@@ -0,0 +1,18 @@
+package kimch321.spring4.sungjukv6.dao;
+
+import kimch321.spring4.sungjukv6.model.SungJukVO;
+
+import java.util.List;
+
+public interface SungJukV6DAO {
+
+    int insertSungJuk(SungJukVO sj);
+
+    List<SungJukVO> selectSungJuk();
+
+    SungJukVO selectOneSungJuk(int sjno);
+
+    int updateSungJuk(SungJukVO sj);
+
+    int deleteSungJuk(int sjno);
+}
